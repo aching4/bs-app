88d9c68fc182f2c2f90be99ffad9934175d0814d
Ashley Ching <aching4@sfsu.edu>
2024-05-11 11:11:30 -0700

    initial commit

---
+++ BillSplitter/BillSplitter/Other/BillSplitterApp.swift
@@ -0,0 +1,20 @@
+//
+//  BillSplitterApp.swift
+//  BillSplitter
+//
+//  Created by Ashley C on 5/11/24.
+//
+
+import SwiftUI
+//import _
+
+@main
+struct BillSplitterApp: App {
+    //init(){_.configure()}
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
