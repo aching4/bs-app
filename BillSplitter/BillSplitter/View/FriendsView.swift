15c1837ec13822472531e053a21aa05476d50695
suza-li <li.suzanna.27@gmail.com>
2024-05-12 11:07:48 -0700

    HomeView
    Started working on Home View

--- BillSplitter/BillSplitter/View/FriendsView.swift
+++ BillSplitter/BillSplitter/View/FriendsView.swift
@@ -3,6 +3,6 @@
 //  BillSplitter
 //
 //  Created by Ashley C on 5/11/24.
-//
+//  Code by Suzanna L on 5/12/24
 
 import Foundation
--- BillSplitter/BillSplitter/View/HomeView.swift
+++ BillSplitter/BillSplitter/View/HomeView.swift
@@ -3,21 +3,38 @@
 //  BillSplitter
 //
 //  Created by Ashley C on 5/11/24.
-//
+//  Code by Suzanna L on 5/12/24
 
 import SwiftUI
 
+//struct HomeView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing:20){
+                NavigationLink(destination: HomeView()) {
+                    Text("Home")
+                        .foregroundStyle(.teal)
+                        .font(.title)
+                }
+            }
+            .navigationBarTitleDisplayMode(/*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
         }
-        .padding()
     }
 }
+
+
 
 #Preview {
     HomeView()
